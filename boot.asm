4e85340bcb498545fce68fcb2db80f547cf80b1c
Dmitry Yakimenko <detunized@gmail.com>
2018-05-25 13:40:02 +0200

    BIOS parameter block

--- boot.asm
+++ boot.asm
@@ -1,4 +1,42 @@
+; 1.44 MB floppy boot sector
+
 ; Entry point
+jmp bootstrap
+nop
+
+; See http://www.ntfs.com/fat-partition-sector.htm
+; See https://www.win.tue.nl/~aeb/linux/fs/fat/fat-1.html
+
+db 'bootplay' ; OEM name
+
+; Extended BIOS parameter block
+dw 512           ; number of bytes per sector
+db 1             ; number of sectors per cluster
+dw 1             ; number of reserved sectors including the boot sector
+db 2             ; number of FATs
+dw 224           ; number of root entries
+dw 2880          ; number of sectors on the disk
+db 0xF0          ; media type (f0 - floppy)
+dw 9             ; number of sectors per FAT
+dw 18            ; number of sectors per track
+dw 2             ; number of heads
+dd 0             ; number of hidden sectors
+dd 0             ; number of large sectors
+db 0             ; physical disk number (0 - A:)
+db 0             ; current head
+db 0x29          ; signature (must be 0x29 for WinNT to recognize)
+dd 0xDEADBEEF    ; volume serial number
+db 'boot2play  ' ; volume label
+db 'FAT12   '    ; file system type
+
+
+; The actual bootstrap code starts here
+bootstrap:
+
+%if bootstrap - $$ != 0x3E
+%error "The bootstrap code must start at 0x3E"
+%endif
+
 mov ax, 0xB800
 mov es, ax
 
